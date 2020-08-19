e6c217e636856f5e565ffd3df4f3dc372bbb934b
rheimers <regineheimers@gmail.com>
2020-08-19 13:23:03 +0200

    Add router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { getTodocategories } from "./api/todocategories";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   const [todocategories, setTodocategories] = useState(null);
@@ -14,11 +15,22 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      {todocategories?.map((todo) => (
-        <div key={todo.id}>{todo.title}</div>
-      ))}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/add">
+          <div>NEW TASK</div>
+        </Route>
+        <Route path="/">
+          <div>ALL TASKS</div>
+        </Route>
+      </Switch>
+      <div className="App">
+        {todocategories?.map((todocategory) => (
+          <div key={todocategory.id}>{todocategory.title}</div>
+        ))}
+      </div>
+    </Router>
   );
 }
+
 export default App;
