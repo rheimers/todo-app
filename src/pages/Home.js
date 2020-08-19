690638dafd5c8950c739a5ca80d5714464e67f7e
rheimers <regineheimers@gmail.com>
2020-08-19 14:01:11 +0200

    Linking of pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { getTodocategories } from "../api/todocategories";
+import { Link } from "react-router-dom";
 
 function Home() {
   const [todocategories, setTodocategories] = useState(null);
@@ -13,6 +14,7 @@ function Home() {
   }, []);
   return (
     <div className="App">
+      <Link to="/add">NEW TASK</Link>
       {todocategories?.map((todocategory) => (
         <div key={todocategory.id}>{todocategory.title}</div>
       ))}
--- src/pages/NewTask.js
+++ src/pages/NewTask.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function NewTask() {
-  return <div>NEW TASK</div>;
+  return (
+    <div>
+      <Link to="/">TASKS</Link>
+      NEW TASK
+    </div>
+  );
 }
 
 export default NewTask;
